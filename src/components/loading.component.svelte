29dc3e2f0ff790da31cb7bb3258478f2fafa7249
= <pmosadeqzadeh@gmail.com>
2025-12-26 16:34:10 +0330

    chore: moved loading to seprate compo

---
+++ src/components/loading.component.svelte
@@ -0,0 +1,4 @@
+<div class="flex h-64 items-center justify-center">
+	<div class="h-12 w-12 animate-spin rounded-full border-t-4 border-blue-500"></div>
+	<p class="ml-4 text-lg">در حال بارگذاری آب و هوا...</p>
+</div>
