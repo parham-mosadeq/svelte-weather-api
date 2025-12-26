e8670fc2782929b254556b5698c59e2feb283dc5
= <pmosadeqzadeh@gmail.com>
2025-12-26 17:32:04 +0330

    fix: changed title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,5 +10,6 @@
 	<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<title>Key Beshinim?</title>
 </svelte:head>
 {@render children()}
