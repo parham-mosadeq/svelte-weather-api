50b0831626ceee39cf112b1cd61b47036a393eca
= <pmosadeqzadeh@gmail.com>
2025-12-26 16:35:53 +0330

    chore: moved table to sep comp

---
+++ src/components/weather-table.component.svelte
@@ -0,0 +1,26 @@
+<script>
+	const { weather } = $props();
+</script>
+
+<table>
+	<thead>
+		<tr>
+			<th>تاریخ</th>
+			<th>روز</th>
+			<th>-/+ دما</th>
+			<th>احتمال باران</th>
+			<th>بشینیم؟</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each weather as day}
+			<tr style:background-color={day.hasRain ? '#e3f2fd' : 'white'}>
+				<td>{day.date}</td>
+				<td>{day.weekday}</td>
+				<td>{day.maxTemp}°C / {day.minTemp}°C</td>
+				<td>{day.precipProb}%</td>
+				<td>{day.hasRain ? 'Yes' : 'No'}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
