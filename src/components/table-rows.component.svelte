1b03b7508cee675f4ef0503492972508f46ebe31
= <pmosadeqzadeh@gmail.com>
2025-12-26 17:43:50 +0330

    fix: updating ui ux

--- src/components/table-rows.component.svelte
+++ src/components/table-rows.component.svelte
@@ -2,27 +2,47 @@
 	import type { WeatherDay } from '../types/weather.types';
 
 	const props: WeatherDay = $props();
+
+	const today = new Date().toLocaleDateString('fa-IR');
+	const isToday = today === props.date;
+
+	let rowBg = $state();
+
+	rowBg = props.hasRain
+		? 'bg-gradient-to-r from-blue-50 to-indigo-50 hover:from-blue-100 hover:to-indigo-100'
+		: 'bg-white hover:bg-gray-50';
+
+	if (isToday) {
+		rowBg = props.hasRain
+			? 'bg-gradient-to-r from-green-50 via-green-100 to-blue-50 hover:from-green-100 hover:to-blue-100'
+			: 'bg-gradient-to-r from-green-50 to-green-100 hover:from-green-100 hover:to-green-200';
+	}
+
+	const precipProbStyles =
+		props.precipProb > 70
+			? 'font-medium text-red-600'
+			: props.precipProb > 30
+				? 'font-medium text-orange-500'
+				: '';
 </script>
 
-<tr
-	class="{props.hasRain
-		? 'bg-linear-to-r from-blue-50 to-indigo-50'
-		: 'bg-white'} border-b border-gray-200 transition-all duration-200 hover:bg-gray-50"
->
-	<td class="px-4 py-4">{props.date}</td>
-	<td class="px-4 py-4 font-medium">{props.weekday}</td>
-	<td class="px-4 py-4">
-		{props.maxTemp}° <span class="text-gray-400">/</span>
-		{props.minTemp}°
+<tr class="{rowBg} border-b border-gray-200 transition-all duration-300 hover:shadow-sm">
+	<td class="px-4 py-4 {isToday ? 'font-bold text-green-800' : ''}">{props.date}</td>
+	<td class="px-4 py-4 font-medium {isToday ? 'text-green-800' : 'text-gray-800'}">
+		{props.weekday}
+
+		{#if isToday}
+			<span class="ml-2 text-sm font-normal text-green-600">(امروز)</span>
+		{/if}
+	</td>
+	<td class="px-4 py-4 {isToday ? 'font-medium text-green-800' : ''}">
+		{props.maxTemp.toLocaleString('fa-IR')}° <span class="text-xl text-gray-900">/</span>
+		{props.minTemp.toLocaleString('fa-IR')}°
 	</td>
 	<td class="px-4 py-4">
-		{#if props.precipProb > 70}
-			<span class="font-medium text-red-600">{props.precipProb}%</span>
-		{:else if props.precipProb > 30}
-			<span class="font-medium text-orange-500">{props.precipProb}%</span>
-		{:else}
-			{props.precipProb}%
-		{/if}
+		<span class={precipProbStyles}>
+			{props.precipProb.toLocaleString('fa-IR')}%
+		</span>
 	</td>
 	<td class="px-4 py-4 text-center font-medium">
 		{#if props.hasRain}
@@ -30,6 +50,9 @@
 				بله 🍻
 				<span class="text-xl">☕</span>
 			</span>
+			{#if isToday}
+				<span class="ml-2 text-sm font-normal text-blue-600">(روز نوشیدنی عالی!)</span>
+			{/if}
 		{:else}
 			<span class="text-gray-500">خیر</span>
 		{/if}
