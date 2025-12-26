67e62869c177ecb46482bdfe9b91614df7bd60a9
= <pmosadeqzadeh@gmail.com>
2025-12-26 16:36:00 +0330

    chore: added table to main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Loading from '../components/loading.component.svelte';
 	import RetryBtn from '../components/retry-btn.component.svelte';
+	import WeatherTable from '../components/weather-table.component.svelte';
 
 	export let data;
 	const weather = data.weather;
@@ -11,28 +12,7 @@
 {#await weather}
 	<Loading />
 {:then weather}
-	<table>
-		<thead>
-			<tr>
-				<th>تاریخ</th>
-				<th>روز</th>
-				<th>-/+ دما</th>
-				<th>احتمال باران</th>
-				<th>بشینیم؟</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each weather as day}
-				<tr style:background-color={day.hasRain ? '#e3f2fd' : 'white'}>
-					<td>{day.date}</td>
-					<td>{day.weekday}</td>
-					<td>{day.maxTemp}°C / {day.minTemp}°C</td>
-					<td>{day.precipProb}%</td>
-					<td>{day.hasRain ? 'Yes' : 'No'}</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+	<WeatherTable {weather} />
 {:catch err}
 	<RetryBtn {err} />
 {/await}
