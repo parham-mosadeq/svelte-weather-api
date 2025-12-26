557bd6965bfedaf1f36df48bd728f1c7568cc97e
= <pmosadeqzadeh@gmail.com>
2025-12-26 17:29:09 +0330

    chore: added heads

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="manifest" href="/manifest.json" />
+	<meta name="theme-color" content="#3b82f6" />
+	<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+</svelte:head>
 {@render children()}
