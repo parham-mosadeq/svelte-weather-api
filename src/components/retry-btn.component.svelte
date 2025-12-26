8cc1a95e4556430662304a0a00b8ba250e0ae728
= <pmosadeqzadeh@gmail.com>
2025-12-26 16:34:20 +0330

    chore: moved error to sep comp

---
+++ src/components/retry-btn.component.svelte
@@ -0,0 +1,14 @@
+<script>
+	const { err } = $props();
+</script>
+
+<div class="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700" role="alert">
+	<strong class="font-bold">خطا!</strong>
+	<span class="block sm:inline"> {err.message || 'بارگذاری ناموفق'}</span>
+	<button
+		class="absolute top-0 right-0 bottom-0 px-4 py-3"
+		onclick={() => window.location.reload()}
+	>
+		تلاش مجدد
+	</button>
+</div>
